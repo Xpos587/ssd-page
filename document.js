702bca17ace4da174acd2dfdb8a9efcd786741f6
Xpos587 <x30827pos@gmail.com>
2022-06-11 13:28:27 +0300

    main

--- document.js
+++
@@ -1,8 +0,0 @@
-module.exports = (username, discriminator, avatar, id) => {
-    console.log(username)
-    console.log(discriminator)
-    console.log(`https://cdn.discordapp.com/avatars/${id}/${avatar}.png?size=2048`)
-
-    const usernameElm = document.getElementById('userDiscriminator')
-    usernameElm.innerText = `${username}#${discriminator}`
-}
